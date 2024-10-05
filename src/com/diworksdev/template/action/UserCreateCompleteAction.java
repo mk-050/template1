e8ba165d9218436ad501ae5058bd63a4681e72e8
mk-050 <yoshikawa.0505@outlook.jp>
2024-10-05 14:49:38 +0900

    4時間目 44ページまで

--- src/com/diworksdev/template/action/UserCreateCompleteAction.java
+++ src/com/diworksdev/template/action/UserCreateCompleteAction.java
@@ -8,7 +8,7 @@ import org.apache.struts2.interceptor.SessionAware;
 import com.diworksdev.template.dao.UserCreateCompleteDAO;
 import com.opensymphony.xwork2.ActionSupport;
 
-public class UserCeateCompleteAction extends ActionSupport implements SessionAware {
+public class UserCreateCompleteAction extends ActionSupport implements SessionAware {
 
 	private String loginUserId;
 	private String loginPassword;
